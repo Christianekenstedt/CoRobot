85a26b440e5ebe731992a3feb36149f45a795c85
christianekenstedt <christian>
2017-10-24 18:39:34 +0200

    fixes

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -12,19 +13,6 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
     }
 }
--- WebApplication1/Models/AMLRequest.cs
+++ WebApplication1/Models/AMLRequest.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace WebApplication1.Models
 {
     public class AMLRequest
     {
-
+        
     }
 }
