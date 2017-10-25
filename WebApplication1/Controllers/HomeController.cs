39ae14fec6a61a11d6d8275a21063b12334cab20
Gurris <gustaf.holmstrom@hotmail.com>
2017-10-25 15:34:36 +0200

    quick fix

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -15,22 +15,6 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        [HttpPost]
-        public ActionResult Index(HttpPostedFileBase file)
-        {
-            // Verify that the user selected a file
-            if (file != null && file.ContentLength > 0)
-            {
-                // extract only the filename
-                var fileName = Path.GetFileName(file.FileName);
-                // store the file inside ~/App_Data/uploads folder
-                var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
-                file.SaveAs(path);
-            }
-            // redirect back to the index action to show the form once again
-            return RedirectToAction("Index");
-        }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
