e4507c91beebb8cfd36b06575333e0b42b70c35a
Gurris <gustaf.holmstrom@hotmail.com>
2017-10-25 15:14:58 +0200

    Controller

---
+++ WebApplication1/Controllers/FileClassController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class FileClassController : Controller
+    {
+        // GET: FileClass
+        public ActionResult Index()
+        {
+            return View(new UploadFileViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(UploadFileViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string filePath = string.Empty;
+                if (model.file != null)
+                {
+                    string path = Server.MapPath("~/Uploads/");
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    filePath = path + Path.GetFileName(model.file.FileName);
+                    string extension = Path.GetExtension(model.file.FileName);
+                    model.file.SaveAs(filePath);
+
+                    //Read the contents of CSV file.
+                    string csvData = System.IO.File.ReadAllText(filePath);
+
+                    //Execute a loop over the rows.
+                    foreach (string row in csvData.Split('\n'))
+                    {
+                        if (!string.IsNullOrEmpty(row))
+                        {
+                            // Sätt breakpoint så ser du texten.
+                            // Jag skapade ett .txt fil som man laddade upp och man kan se texten i den
+                        }
+                    }
+                }
+                
+            }
+            
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ WebApplication1/Models/FileClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class FileClass
+    {
+        public string url { get; set; }
+        public HttpPostedFile file { get; set; }
+    }
+
+    public class UploadFileViewModel
+    {
+        //[Required]
+        [Display(Name = "URL")]
+        public string url { get; set; }
+
+        //[Required]
+        [Display(Name = "File")]
+        [DataType(DataType.Upload)]
+        public HttpPostedFileBase file { get; set; }
+        
+
+    }
+
+}
